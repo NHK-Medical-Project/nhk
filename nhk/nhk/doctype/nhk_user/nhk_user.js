4c2e32da21736b7ac35d217a7c3849751e6b3040
Mohan@360ithub <mohan@360ithub.com>
2024-05-08 13:29:58 +0530

    from local 2.5.24

--- nhk/nhk/doctype/nhk_user/nhk_user.js
+++ nhk/nhk/doctype/nhk_user/nhk_user.js
@@ -2,111 +2,121 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("NHK User", {
-    before_submit: function (frm) {
-        // Perform actions after form submission
-
-        console.log("User is Created");
-
-        // Reload the form
-        // frm.reload_doc();
-        location.reload(true);
+    refresh: function(frm) {
+        // Set custom query for the "role" field
+        frm.fields_dict['role'].get_query = function(doc) {
+            return {
+                filters: {
+                    "custom_nhk_roles": 1
+                }
+            };
+        };
     },
+    // before_submit: function (frm) {
+    //     // Perform actions after form submission
+
+    //     console.log("User is Created");
+
+    //     // Reload the form
+    //     // frm.reload_doc();
+    //     location.reload(true);
+    // },
     onload: function (frm) {
         // Clear existing roles wrapper content
-        frm.fields_dict.nhk_roles.$wrapper.empty();
+        // frm.fields_dict.nhk_roles.$wrapper.empty();
 
-        if (frm.doc.docstatus === 1) {
-            frappe.call({
-                method: 'nhk.nhk.doctype.nhk_user.nhk_user.get_nhk_roles_and_core_user_roles',
-                args: {
-                    u_id: frm.doc.name,
-                },
-                callback: function (r) {
-                    var nhkRoles = r.message.nhk_roles;
-                    var coreUserRoles = r.message.core_user_roles;
+        // if (frm.doc.docstatus === 1) {
+        //     frappe.call({
+        //         method: 'nhk.nhk.doctype.nhk_user.nhk_user.get_nhk_roles_and_core_user_roles',
+        //         args: {
+        //             u_id: frm.doc.name,
+        //         },
+        //         callback: function (r) {
+        //             var nhkRoles = r.message.nhk_roles;
+        //             var coreUserRoles = r.message.core_user_roles;
 
-                    // Create the role editor div
-                    var roleEditorDiv = $('<div class="role-editor">').appendTo(frm.fields_dict.nhk_roles.wrapper);
+        //             // Create the role editor div
+        //             var roleEditorDiv = $('<div class="role-editor">').appendTo(frm.fields_dict.nhk_roles.wrapper);
 
-                    // Create the inner div with class "frappe-control"
-                    var innerDiv = $('<div class="frappe-control" data-fieldtype="MultiCheck" data-fieldname="roles">')
-                        .append($('<span class="tooltip-content">').text("roles"))
-                        .appendTo(roleEditorDiv);
+        //             // Create the inner div with class "frappe-control"
+        //             var innerDiv = $('<div class="frappe-control" data-fieldtype="MultiCheck" data-fieldname="roles">')
+        //                 .append($('<span class="tooltip-content">').text("roles"))
+        //                 .appendTo(roleEditorDiv);
 
-                    // Create the checkbox options div
-                    var checkboxOptionsDiv = $('<div class="checkbox-options">')
-                        .append($('<div class="load-state text-muted small">').text("Loading...").hide())
-                        .appendTo(innerDiv);
+        //             // Create the checkbox options div
+        //             var checkboxOptionsDiv = $('<div class="checkbox-options">')
+        //                 .append($('<div class="load-state text-muted small">').text("Loading...").hide())
+        //                 .appendTo(innerDiv);
 
-                    // Create a container for the columns
-                    var columnsContainer = $('<div class="columns-container" style="display:flex;justify-content:space-around;">')
-                        .appendTo(checkboxOptionsDiv);
+        //             // Create a container for the columns
+        //             var columnsContainer = $('<div class="columns-container" style="display:flex;justify-content:space-around;">')
+        //                 .appendTo(checkboxOptionsDiv);
 
-                    // Calculate the number of roles per column
-                    var rolesPerColumn = Math.ceil(nhkRoles.length / 3);
+        //             // Calculate the number of roles per column
+        //             var rolesPerColumn = Math.ceil(nhkRoles.length / 3);
 
-                    // Create three columns
-                    for (var i = 0; i < 3; i++) {
-                        // Create a column div
-                        var columnDiv = $('<div class="column" >').appendTo(columnsContainer);
+        //             // Create three columns
+        //             for (var i = 0; i < 3; i++) {
+        //                 // Create a column div
+        //                 var columnDiv = $('<div class="column" >').appendTo(columnsContainer);
 
-                        // Populate roles in the column
-                        for (var j = i * rolesPerColumn; j < (i + 1) * rolesPerColumn && j < nhkRoles.length; j++) {
-                            var role = nhkRoles[j];
-                            var isChecked = coreUserRoles.some(userRole => userRole.role === role);
+        //                 // Populate roles in the column
+        //                 for (var j = i * rolesPerColumn; j < (i + 1) * rolesPerColumn && j < nhkRoles.length; j++) {
+        //                     var role = nhkRoles[j];
+        //                     var isChecked = coreUserRoles.some(userRole => userRole.role === role);
 
-                            // Create checkbox div
-                            var checkboxDiv = $('<div class="checkbox unit-checkbox">')
-                                .append($('<label title="">')
-                                    .append($('<input class ="nhk_roles" type="checkbox" data-unit="' + role + '"' + (isChecked ? ' checked' : '') + '>'))
-                                    .append($('<span class="label-area" data-unit="' + role + '">').text(role))
-                                )
-                                .appendTo(columnDiv);
-                        }
-                    }
+        //                     // Create checkbox div
+        //                     var checkboxDiv = $('<div class="checkbox unit-checkbox">')
+        //                         .append($('<label title="">')
+        //                             .append($('<input class ="nhk_roles" type="checkbox" data-unit="' + role + '"' + (isChecked ? ' checked' : '') + '>'))
+        //                             .append($('<span class="label-area" data-unit="' + role + '">').text(role))
+        //                         )
+        //                         .appendTo(columnDiv);
+        //                 }
+        //             }
 
-                    // Create button div
-                    var buttonDiv = $('<div class="button-div" style="text-align: right;">').appendTo(checkboxOptionsDiv);
+        //             // Create button div
+        //             var buttonDiv = $('<div class="button-div" style="text-align: right;">').appendTo(checkboxOptionsDiv);
 
-                    // Create update button
-                    var updateButton = $('<button class="btn btn-primary" style="margin:20px;">Update Roles</button>')
-                        .appendTo(buttonDiv)
-                        .on('click', function () {
-                            frappe.confirm(
-                                "Are you sure you want to update core user roles?",
-                                function () {
-                                    var rolesHTML = $('#page-nhk\\ User [data-fieldname="roles_for_user"]').html();
-                                    var rolesData = extractRolesFromHTML(rolesHTML);
+        //             // Create update button
+        //             var updateButton = $('<button class="btn btn-primary" style="margin:20px;">Update Roles</button>')
+        //                 .appendTo(buttonDiv)
+        //                 .on('click', function () {
+        //                     frappe.confirm(
+        //                         "Are you sure you want to update core user roles?",
+        //                         function () {
+        //                             var rolesHTML = $('#page-nhk\\ User [data-fieldname="roles_for_user"]').html();
+        //                             var rolesData = extractRolesFromHTML(rolesHTML);
 
-                                    frappe.call({
-                                        method: 'nhk.nhk.doctype.nhk_user.nhk_user.update_core_user_roles',
-                                        args: {
-                                            u_id: frm.doc.name,
-                                            roles: JSON.stringify(rolesData),
-                                        },
-                                        callback: function (r) {
-                                            if (r.message) {
-                                                frappe.msgprint(__('User roles updated successfully.'));
-                                                frm.refresh();
-                                            } else {
-                                                frappe.msgprint("Server Side Error");
-                                            }
-                                        }
-                                    });
-                                },
-                                function () {
-                                    console.log("Update Roles canceled.");
-                                }
-                            );
-                        });
-                },
-            });
-        }
-        else{
-            var targetDiv = $('.section-head:contains("nhk Roles")');
-            // Empty the content of the targeted div
-            targetDiv.empty();
-        }
+        //                             frappe.call({
+        //                                 method: 'nhk.nhk.doctype.nhk_user.nhk_user.update_core_user_roles',
+        //                                 args: {
+        //                                     u_id: frm.doc.name,
+        //                                     roles: JSON.stringify(rolesData),
+        //                                 },
+        //                                 callback: function (r) {
+        //                                     if (r.message) {
+        //                                         frappe.msgprint(__('User roles updated successfully.'));
+        //                                         frm.refresh();
+        //                                     } else {
+        //                                         frappe.msgprint("Server Side Error");
+        //                                     }
+        //                                 }
+        //                             });
+        //                         },
+        //                         function () {
+        //                             console.log("Update Roles canceled.");
+        //                         }
+        //                     );
+        //                 });
+        //         },
+        //     });
+        // }
+        // else{
+        //     var targetDiv = $('.section-head:contains("nhk Roles")');
+        //     // Empty the content of the targeted div
+        //     targetDiv.empty();
+        // }
     },
 });
 
--- nhk/nhk/doctype/nhk_user/nhk_user.py
+++ nhk/nhk/doctype/nhk_user/nhk_user.py
@@ -7,14 +7,15 @@ import json
 
 class NHKUser(Document):
     def on_submit(self):
-        # Check if a core user with the same email exists
         existing_core_user = frappe.get_all("User", filters={"email": self.email})
+        role_profile_names = [role_profile.role_profile for role_profile in self.role]
+
         if not existing_core_user:
             # Create a new core user if it doesn't exist
             core_user = frappe.get_doc({
                 "doctype": "User",
                 "email": self.email,
-                "full_name": self.full_name,
+                # "full_name": self.full_name,
                 "first_name": self.first_name,
                 "last_name": self.last_name,
                 "middle_name": self.middle_name,
@@ -24,67 +25,171 @@ class NHKUser(Document):
                 "send_welcome_email": self.send_welcome_email,
                 "mobile_no": self.mobile_no,
                 "new_password": self.password,
-                "module_profile":"No Module"
+                "module_profile": "No Module",
             })
+
+            # Add each role profile to the core user's role_profiles field
+            for role_profile in role_profile_names:
+                core_user.append("role_profiles", {
+                    "role_profile": role_profile
+                })
+                
             core_user.insert()
-            return
+
+            # Once user creation is successful, create Employee record
+            create_employee_from_user(core_user)
+            if "NHK Sales" in role_profile_names:
+                create_sales_person(core_user)
+
         return
 
+    def before_save(self):
+        name_list=[self.first_name,self.middle_name,self.last_name]
+        name_list = [value for value in name_list if value is not None]
+        self.full_name=" ".join(name_list)
+
     def before_update_after_submit(self):
-        # Update core user information on LSAUser update
+        # Extract role profile names from UserRoleProfile objects
+        role_profile_names = [role_profile.role_profile for role_profile in self.role]
+
+        # Update core user information on NHK User update
         core_users = frappe.get_all("User", filters={"email": self.email}, fields=["name"])
+
         if core_users:
             core_user = frappe.get_doc("User", core_users[0].name)
-            core_user.full_name = self.full_name
-            core_user.first_name = self.first_name
-            core_user.last_name = self.last_name
-            core_user.middle_name = self.middle_name
-            core_user.gender = self.gender
-            core_user.enabled = self.enabled
-            core_user.birth_date = self.date_of_birth
-            core_user.send_welcome_email = self.send_welcome_email
-            core_user.mobile_no = self.mobile_no
+            core_user.update({
+                "full_name": self.full_name,
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "middle_name": self.middle_name,
+                "gender": self.gender,
+                "enabled": self.enabled,
+                "birth_date": self.date_of_birth,
+                "send_welcome_email": self.send_welcome_email,
+                "mobile_no": self.mobile_no
+            })
+
+            # Update password if not "NULL"
             if self.password != "NULL":
                 core_user.new_password = self.password
+
+            # Assign role profiles to the core user
+            core_user.set("role_profiles", [])
+            for role_profile_name in role_profile_names:
+                core_user.append("role_profiles", {
+                    "role_profile": role_profile_name
+                })
+            if "NHK Sales" in role_profile_names:
+                create_sales_person(core_user)
+            else:
+                employee_id = frappe.get_value("Employee", {"user_id": self.email}, "name")
+                delete_sales_person(employee_id)
             core_user.save()
-            return
-        return
+
 
     def on_trash(self):
         # Handle user deletion
         user = frappe.get_all("User", filters={"email": self.email})
+        employee_id = frappe.get_value("Employee", {"user_id": self.email}, "name")
+
         if user:
+            delete_sales_person(employee_id)
+            # Also, delete the associated Employee record if it exists
+            delete_employee_from_user(user[0].name)
             # pass  # Uncomment the next line if you want to delete the associated core user
             frappe.delete_doc("User", user[0].name)
+            
+           
 
+
+def create_sales_person(user):
+    # Get the Employee ID of the newly created employee
+    employee_id = frappe.get_value("Employee", {"user_id": user.name}, "name")
+    # Check if the user already has a Sales Person record
+    existing_sales_person = frappe.get_all("Sales Person", filters={"employee": employee_id})
+    if existing_sales_person:
+        return  # Sales Person record already exists for the user
+    
+    # Create a new Sales Person record
+    sales_person = frappe.get_doc({
+        "doctype": "Sales Person",
+        "sales_person_name": user.full_name,
+        # "user_id": user.name,
+        "employee": employee_id
+        # Add other relevant fields
+    })
+    sales_person.insert()
+
+
+
+def create_employee_from_user(user):
+    employee = frappe.get_doc({
+        "doctype": "Employee",
+        "first_name": user.first_name,
+        "middle_name": user.middle_name,
+        "last_name":user.last_name,
+        "user_id": user.name,
+        "create_user_permission":0
+        # Add other relevant fields from user or other sources
+    })
+    employee.insert()
+
+
+def delete_employee_from_user(user_id):
+    # Find the Employee record associated with the given user_id
+    employee = frappe.get_all("Employee", filters={"user_id": user_id})
+    if employee:
+        # Delete the Employee record
+        frappe.delete_doc("Employee", employee[0].name)
+
+
+
+def delete_sales_person(employee_id):
+    # Assuming the sales person record is linked to the Employee record
+    sales_person = frappe.get_all("Sales Person", filters={"employee": employee_id})
+    if sales_person:
+        frappe.delete_doc("Sales Person", sales_person[0].name)
 
 @frappe.whitelist()
 def user_to_nhkuser():
-    # Sync core user data to LSAUser
+    # Sync core user data to NHK User
     core_users = frappe.get_all("User", fields=["name", "email", "full_name", "first_name", "last_name",
-                                                "middle_name", "gender", "enabled", "birth_date", "send_welcome_email",
-                                                "mobile_no", "new_password"])
+                                                "middle_name", "gender", "enabled", "birth_date", 
+                                                "send_welcome_email", "mobile_no", "new_password"])
+
     for core_user in core_users:
-        lsa_user = frappe.get_all("NHK User", filters={"email": core_user.email})
-        if core_user.first_name in ["Guest","Administrator","Account"]:
+        if core_user.first_name in ["Guest", "Administrator", "Account", "Pankaj"]:
+            continue
+
+        # Fetching role profiles for the current user
+        role_profiles = frappe.get_all("User Role Profile", filters={"parent": core_user.name},
+                                       fields=["role_profile"])
+        role_profile_names = [role["role_profile"] for role in role_profiles]
+        
+        lsa_user = frappe.get_all("NHK User", filters={"email": core_user.email}, fields=["name"])
+        
+        if any(core_user.email == user_info['name'] for user_info in lsa_user):
             continue
         elif lsa_user:
-            # Update existing LSAUser
+            # Update existing NHK User
             existing_lsa_user = frappe.get_doc("NHK User", lsa_user[0].name)
-            existing_lsa_user.full_name = core_user.full_name
-            existing_lsa_user.first_name = core_user.first_name
-            existing_lsa_user.last_name = core_user.last_name
-            existing_lsa_user.middle_name = core_user.middle_name
-            existing_lsa_user.gender = core_user.gender
-            existing_lsa_user.enabled = core_user.enabled
-            existing_lsa_user.date_of_birth = core_user.birth_date
-            existing_lsa_user.send_welcome_email = core_user.send_welcome_email
-            existing_lsa_user.mobile_no = core_user.mobile_no
+            existing_lsa_user.update({
+                "full_name": core_user.full_name,
+                "first_name": core_user.first_name,
+                "last_name": core_user.last_name,
+                "middle_name": core_user.middle_name,
+                "gender": core_user.gender,
+                "enabled": core_user.enabled,
+                "date_of_birth": core_user.birth_date,
+                "send_welcome_email": core_user.send_welcome_email,
+                "mobile_no": core_user.mobile_no,
+                "role": [{"role_profile": role_profile} for role_profile in role_profile_names]
+            })
             existing_lsa_user.save()
         else:
-            # Create a new LSAUser if it doesn't exist
-            new_lsa_user_fields = {
-                "doctype": "NHK User",
+            # Create a new NHK User if it doesn't exist
+            new_lsa_user = frappe.new_doc("NHK User")
+            new_lsa_user.update({
                 "email": core_user.email,
                 "full_name": core_user.full_name,
                 "first_name": core_user.first_name,
@@ -96,43 +201,43 @@ def user_to_nhkuser():
                 "send_welcome_email": core_user.send_welcome_email,
                 "mobile_no": core_user.mobile_no,
                 "password": "NULL",
-                "lsa_ops_manager": 1,
-            }
-            new_lsa_user = frappe.get_doc(new_lsa_user_fields)
+                "role": [{"role_profile": role_profile} for role_profile in role_profile_names]
+            })
             new_lsa_user.insert()
             new_lsa_user.submit()
     return {"status": "Users Synced"}
 
-@frappe.whitelist()
-def get_all_nhk_roles():
-    # Fetch all LSA roles
-    roles = frappe.get_all("Role", filters={"name": ("not in", frappe.permissions.AUTOMATIC_ROLES),
-                                             "disabled": 0, "nhk_roles": 1}, order_by="name")
-    roles = [role.get("name") for role in roles]
-    return {"roles": roles}
 
-@frappe.whitelist()
-def get_nhk_roles_and_core_user_roles(u_id):
-    try:
-        # Fetch LSA roles and roles assigned to the core user
-        nhk_roles = get_all_nhk_roles()["roles"]
-        core_user_roles = frappe.get_doc('User', u_id).get('roles')
-        return {'nhk_roles': nhk_roles, 'core_user_roles': core_user_roles}
-    except Exception as e:
-        frappe.log_error(frappe.get_traceback(), _('Failed to fetch roles'))
-        return None
+# @frappe.whitelist()
+# def get_all_nhk_roles():
+#     # Fetch all LSA roles
+#     roles = frappe.get_all("Role", filters={"name": ("not in", frappe.permissions.AUTOMATIC_ROLES),
+#                                              "disabled": 0, "nhk_roles": 1}, order_by="name")
+#     roles = [role.get("name") for role in roles]
+#     return {"roles": roles}
+
+# @frappe.whitelist()
+# def get_nhk_roles_and_core_user_roles(u_id):
+#     try:
+#         # Fetch LSA roles and roles assigned to the core user
+#         nhk_roles = get_all_nhk_roles()["roles"]
+#         core_user_roles = frappe.get_doc('User', u_id).get('roles')
+#         return {'nhk_roles': nhk_roles, 'core_user_roles': core_user_roles}
+#     except Exception as e:
+#         frappe.log_error(frappe.get_traceback(), _('Failed to fetch roles'))
+#         return None
 
 
-@frappe.whitelist()
-def update_core_user_roles(u_id, roles):
-    try:
-        # Update core user roles
-        roles = json.loads(roles)
-        core_user = frappe.get_doc('User', u_id)
-        core_user.roles = []
-        core_user.add_roles(*roles)
-        core_user.save()
-        return True
-    except Exception as e:
-        frappe.log_error(frappe.get_traceback(), _('Core User Roles Update Failed'))
-        return False
+# @frappe.whitelist()
+# def update_core_user_roles(u_id, roles):
+#     try:
+#         # Update core user roles
+#         roles = json.loads(roles)
+#         core_user = frappe.get_doc('User', u_id)
+#         core_user.roles = []
+#         core_user.add_roles(*roles)
+#         core_user.save()
+#         return True
+#     except Exception as e:
+#         frappe.log_error(frappe.get_traceback(), _('Core User Roles Update Failed'))
+#         return False
