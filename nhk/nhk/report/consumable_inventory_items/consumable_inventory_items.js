d4f3de031d6503a0ed5db8a09c78c1dfc8ebf6f7
Mohan Madhan <mohan@360ithub.com>
2024-10-30 17:03:13 +0530

    consumable_inventory_items

--- nhk/nhk/report/consumable_inventory_items/consumable_inventory_items.js
+++ nhk/nhk/report/consumable_inventory_items/consumable_inventory_items.js
@@ -7,7 +7,17 @@ frappe.query_reports["Consumable Inventory Items"] = {
 			"options": "Item",
 			"width": 150,
 			"default": null,
-			"reqd": 0
+			"reqd": 0,
+			"get_query": function() {
+				return {
+					"filters": {
+						"item_group": "Sales",
+						"is_stock_item": 1,
+						"has_serial_no":0,
+						"has_batch_no":0
+					}
+				};
+			}
 		}
 	]
 };
